607b460981cd5e091cc7c3444e3e4a90ff587e4a
cyberseckenny <kennycs@protonmail.com>
2024-02-02 20:25:21 -0500

    Added PromptConstructorTest and template prompt and answers

---
+++ app/src/test/java/me/grocery/grocerylist/PromptConstructorTest.java
@@ -0,0 +1,9 @@
+package me.grocery.grocerylist;
+
+import org.junit.Test;
+
+public class PromptConstructorTest {
+    private final String INITIAL_PROMPT = "What kind of meal plan or diet are you looking to create?";
+    private final String INITIAL_ANSWER = "I am looking for a diet that allows me to lose weight " +
+            "while still having energy to function during the day.";
+}
